6e1ffbeaf16a082d4c74bd1dd5a5581f8dee9040
Hosain <cryptichasan925@gmail.com>
2025-07-26 18:24:50 +0600

    attendance interface done

---
+++ src/app/modules/generic/attendance/interface/attendance.interface.ts
@@ -0,0 +1,16 @@
+import { Types } from 'mongoose';
+
+export type AttendanceStatus = 'present' | 'absent' | 'late' | 'leave';
+
+export interface IAttendance {
+  organization: Types.ObjectId;
+  student: Types.ObjectId;
+  class: number;
+  session: number;
+  date: Date;
+  status: AttendanceStatus;
+  department: Types.ObjectId;
+  group: string;
+  createdAt?: Date;
+  updatedAt?: Date;
+}
