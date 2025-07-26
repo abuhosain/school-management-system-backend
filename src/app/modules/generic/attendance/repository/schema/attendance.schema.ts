2cdf355a61820ffe6e12062322508fecb1737bd7
Hosain <cryptichasan925@gmail.com>
2025-07-26 18:27:28 +0600

    attendance schema done

---
+++ src/app/modules/generic/attendance/repository/schema/attendance.schema.ts
@@ -0,0 +1,33 @@
+import { Schema, model } from 'mongoose';
+import { IAttendance } from '../../interface/attendance.interface';
+
+const attendanceSchema = new Schema<IAttendance>(
+  {
+    organization: {
+      type: Schema.Types.ObjectId,
+      ref: 'Organization',
+      required: true,
+    },
+    student: { type: Schema.Types.ObjectId, ref: 'Student', required: true },
+    class: { type: Number, required: true },
+    session: { type: Number, required: true },
+    date: { type: Date, required: true },
+    status: {
+      type: String,
+      enum: ['present', 'absent', 'late', 'leave'],
+      default: 'absent',
+    },
+    department: {
+      type: Schema.Types.ObjectId,
+      ref: 'Department',
+      required: true,
+    },
+    group: { type: String, required: true, trim: true },
+  },
+  {
+    timestamps: true,
+    versionKey: false,
+  },
+);
+
+export const Attendance = model<IAttendance>('Attendance', attendanceSchema);
