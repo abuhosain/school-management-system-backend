1ef947655152d39bc9c80257049380aed0c1d233
Hosain <cryptichasan925@gmail.com>
2025-07-27 16:03:25 +0600

    teacher updated

--- src/app/modules/generic/teacher/inteface/tacher.interface.ts
+++ src/app/modules/generic/teacher/inteface/tacher.interface.ts
@@ -1,9 +1,14 @@
-import { Types } from "mongoose";
+import { Types } from 'mongoose';
 
 export interface ITeacher {
   user: Types.ObjectId;
   organization: Types.ObjectId;
   department: Types.ObjectId;
+  name: string;
+  email: string;
+  phone: number;
+  ephone: number;
+  profilePicture?: string;
   designation: string;
   join_date: Date;
 }
