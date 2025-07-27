fd30f8fb01b58f68ad93a55cc025818abf0ef8d7
Hosain <cryptichasan925@gmail.com>
2025-07-27 19:20:54 +0600

    auth controller

--- src/app/modules/shared/auth/controller/auth.controller.ts
+++ src/app/modules/shared/auth/controller/auth.controller.ts
@@ -1,1 +1,19 @@
- 
+import catchAsync from '../../../../utils/catchAsynch';
+import sendResponse from '../../../../utils/sendResponse';
+import { AuthServices } from '../services/auth.services';
+import httpStatus from 'http-status';
+
+const createOrganization = catchAsync(async (req, res) => {
+  const organization = req.body;
+  const result = await AuthServices.createOrganization(organization);
+  sendResponse(res, {
+    statusCode: httpStatus.OK,
+    success: true,
+    message: 'School Website created successfully',
+    data: result,
+  });
+});
+
+export const AuthController = {
+  createOrganization,
+};
