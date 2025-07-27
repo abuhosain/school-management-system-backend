41fb7e2c3d3d79d58b8f52430b92f06b9f4c59ae
Hosain <cryptichasan925@gmail.com>
2025-07-27 20:12:07 +0600

    student create done

--- src/app/modules/global/user/services/user.services.ts
+++ src/app/modules/global/user/services/user.services.ts
@@ -1,4 +1,4 @@
-import { email } from 'zod';
+import mongoose from 'mongoose';
 import AppError from '../../../../errors/AppError';
 import { IStudent } from '../../../generic/student/interface/student.interface';
 import { User } from '../repository/schema/user.schema';
@@ -9,32 +9,50 @@ import { Student } from '../../../generic/student/repository/schema/student.sche
 import { TImageFile } from '../../../../interface/image.interface';
 
 const createStudent = async (studentData: IStudent, file: TImageFile) => {
-  const isExistingStudent = await User.isUserExistsByEmail(studentData.email);
-  if (isExistingStudent) {
-    throw new AppError(
-      httpStatus.CONFLICT,
-      'Student with this email already exists',
+  const session = await mongoose.startSession();
+
+  try {
+    session.startTransaction();
+
+    const isExistingStudent = await User.isUserExistsByEmail(studentData.email);
+    if (isExistingStudent) {
+      throw new AppError(
+        httpStatus.CONFLICT,
+        'Student with this email already exists',
+      );
+    }
+
+    const password = studentData.roll_no.toString();
+    const hashedPassword = await bcrypt.hash(password, 12);
+
+    const userData = {
+      email: studentData.email,
+      password: hashedPassword,
+      role: USER_ROLE.student,
+      name: studentData.name,
+      profilePicture: file?.path,
+    };
+
+    const user = await User.create([userData], { session });
+
+    const student = await Student.create(
+      [{
+        ...studentData,
+        user: user[0]._id,
+        profilePicture: file.path,
+      }],
+      { session }
     );
+
+    await session.commitTransaction();
+    session.endSession();
+
+    return student[0];
+  } catch (error) {
+    await session.abortTransaction();
+    session.endSession();
+    throw error;
   }
-  const password = studentData.roll_no.toString();
-  const hashedPassword = await bcrypt.hash(password, 12);
-  const userData = {
-    email: studentData.email,
-    password: hashedPassword,
-    role: USER_ROLE.student,
-    name: studentData.name,
-    profilePicture: file?.path,
-  };
-
-  const user = await User.create(userData);
-
-  const student = await Student.create({
-    ...studentData,
-    user: user._id,
-    profilePicture: file.path,
-  });
-
-  return student;
 };
 
 export const UserServices = {
