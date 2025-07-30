4205e49da4409a17911e02b36fd4be32be86b086
Hosain <cryptichasan925@gmail.com>
2025-07-31 02:35:31 +0600

    updated

--- src/app/modules/generic/student/validations/student.validations.ts
+++ src/app/modules/generic/student/validations/student.validations.ts
@@ -1,15 +1,14 @@
 import { z } from 'zod';
 import { Types } from 'mongoose';
-
-const objectId = z.instanceof(Types.ObjectId);
+ 
 
 export const studentZodSchema = z.object({
   body: z.object({
-    department: objectId,
+    department: z.string().min(1, 'Department is required'),
     name: z.string().min(1, 'Name is required'),
     email: z.string().email('Invalid email format').min(1, 'Email is required'),
-    phone: z.number().int().min(1, 'Phone number is required'),
-    ephone: z.number().int().min(1, 'Emergency phone number is required'),
+    phone: z.number().min(1, 'Phone number is required'),
+    ephone: z.number().min(1, 'Emergency phone number is required'),
     profilePicture: z.string().optional(),
     roll_no: z.number(),
     reg_no: z.number(),
@@ -17,7 +16,7 @@ export const studentZodSchema = z.object({
     group: z.string().optional(),
     session: z.number(),
     gender: z.enum(['Male', 'Female']),
-    dob: z.date().min(1, 'Date of birth is required'),  
+    dob: z.string().min(1, 'Date of birth is required'),  
     blood_group: z.string().min(1, 'Blood group is required'),
   }),
 });
